8b3d641f0be3f82a5ad9e6edc4b79f4b273aa33e
Claudia Alonso <59338779+Claudia-Alonso@users.noreply.github.com>
2021-09-20 15:46:24 +0100

    Update drone_bytes.py

--- first_demo/radio/drone_bytes.py
+++ first_demo/radio/drone_bytes.py
@@ -15,6 +15,7 @@ while True:
         display.scroll("In")
         print(str(incoming))
         print(len(incoming))
-        split_byte_list = [incoming[i] for i in range (0, len(incoming))]
+        split_byte_list = [incoming[i] for i in range (0, len(incoming))] # Parsing the incoming 1 byte wide and putting it into a list
+        # https://stackoverflow.com/questions/20024490/how-to-split-a-byte-string-into-separate-bytes-in-python
         for x in split_byte_list:
             print(x)
