42e55b3c337be5b33bf0e89d1f5386890c841161
Daniel Flood <62947583+dflood99@users.noreply.github.com>
2021-11-12 19:20:23 +0000

    Create drone.py
    The drone seems to hold its height fine only using a proportional controller in the z-direction.
    Goes out of control in the x-y directions however PID for these has been disabled.
    My concern is that we may also need to take into account the drones orientation as well as its distance from the centre of the joystick.
    May be possible to have a working demo by the end of week 10 building on this code.

---
+++ week9/working-code/drone.py
@@ -0,0 +1,151 @@
+# Add your Python code here. E.g.
+from microbit import *
+import radio
+from math import *
+import micropython
+import time
+
+uart.init(baudrate=115200, bits=8, parity=None, stop=1, tx=pin1, rx=pin8)
+#uart.init(baudrate=115200, bits=8, parity=None, stop=1, tx=None, rx=None)
+
+radio.on()
+radio.config(length=251)
+radio.config(channel=47)
+radio.config(queue=1)
+micropython.kbd_intr(-1)
+
+x = 0
+y = 0
+z = 0
+a = 0
+t_flag = 0
+
+incoming = 0
+
+buf = bytearray(16)
+
+# function for splitting up incoming strings and assigning values for p,a,r,t,y
+def receiver():
+    global x, y, z, a, t_flag
+
+    split_string = incoming.split(",")
+   
+    #print(incoming)
+
+    x = int(split_string[0])
+    y = int(split_string[1])
+    z = int(split_string[2])
+    a = int(split_string[3])
+    t_flag = int(split_string[4])
+   
+    #print("x: ", x, " y: ", y, " z: ", z, " a: ", a, "t_flag:", t_flag)
+   
+    if a > 0:
+        display.set_pixel(0, 0, 9)
+    else:
+        display.set_pixel(0, 0, 0)
+   
+# PID for X-direction control
+xE = 0
+xI = 0
+def xPID(xKp, xKi, xKd):
+    global xE, xI
+    error = 0 - x
+    
+    xP = error
+    xI += error
+    xD = error - xE
+    
+    xE = error
+    
+    roll = xKp*xP + xKi*xI + xKd*xD
+    
+    return roll
+    
+# PID for Y-direction control
+yE = 0
+yI = 0
+def yPID(yKp, yKi, yKd):
+    global yE, yI
+    error = 0 - y
+    
+    yP = error
+    yI += error
+    yD = error - yE
+    
+    yE = error
+    
+    pitch = yKp*yP + yKi*yI + yKd*yD
+    
+    return pitch  
+    
+# PID for Z-direction control
+zE = 0
+zI = 0
+def zPID(zKp, zKi, zKd):
+    global zE, zI
+    error = 50 - z
+    #print("Error: ", error)
+    zP = error
+    zI += error
+    zD = error - zE
+    
+    zE = error
+    
+    throttle = zKp*zP + zKi*zI + zKd*zD
+    
+    if throttle > 95:
+        throttle = 95
+    if throttle < 0:
+        throttle = 0
+    return throttle  
+ 
+   
+def driver(pitch, roll, throttle):
+
+    p_int = int( 3.5 * pitch + 512)
+    r_int = int( 3.5 * roll  + 521)
+    t_int = int((512 * throttle)/50)
+    y_int = 512
+   
+    arm = 900*a
+   
+    #print("P: ", p_int, " A: ", arm, " R: ", r_int, " T: ", t_int, " Y: ", y_int)
+
+    
+    buf[0] = 0
+    buf[1] = 0x01
+    buf[2] = (0 << 2) | ((r_int >> 8) & 3)
+    buf[3] = r_int & 255
+    buf[4] = (1 << 2) | ((p_int >> 8) & 3)
+    buf[5] = p_int & 255
+    buf[6] = (2 << 2) | ((t_int >> 8) & 3)
+    buf[7] = t_int & 255
+    buf[8] = (3 << 2) | ((512 >> 8) & 3)
+    buf[9] = 512 & 255
+    buf[10] = (4 << 2) | ((arm >> 8) & 3)
+    buf[11] = arm & 255
+    buf[12] = (5 << 2) | ((225 >> 8) & 3)
+    buf[13] = 225 & 255
+    buf[14] = (6 << 2) | ((0 >> 8) & 3)
+    buf[15] = 0 & 255
+   
+    uart.write(buf)
+   
+while True:
+   
+    incoming = radio.receive()
+   
+    if incoming:
+        receiver()
+        # roll = 0 #xPID(1, 0, 0)
+        # pitch = 0  #yPID(1, 0, 0)
+        if t_flag == 1:
+            throttle = zPID(2, 0, 0)
+        else:
+            throttle = 0
+        #print("Throttle: ", throttle)
+        driver(x, y, throttle)
+        sleep(50)
+    else:
+        print("No Incoming Data")
