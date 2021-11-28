a8bebc4bcc87418d4b31b232aeeb537be1410632
Daniel Flood <62947583+dflood99@users.noreply.github.com>
2021-11-28 14:56:47 +0000

    ACK system implemented

---
+++ week11_sunday/2_monitor.py
@@ -0,0 +1,27 @@
+# Add your Python code here. E.g.
+from microbit import *
+import radio
+import micropython
+
+radio.on()
+radio.config(length=251)
+radio.config(channel=47)
+radio.config(queue=1)
+
+while True:
+    incoming = radio.receive()
+    
+    if incoming:
+        string = incoming.split(",")
+        address = int(string[0])
+        if address == 2:
+            if string[1] == '0':
+                pitch = int(string[1])
+                roll  = int(string[2])
+                print("Pitch: ", pitch, " Roll: ", roll)
+                radio.send("0,1,1")
+                sleep(3000)
+            else:
+                radio.send("0,1,0")
+        else:
+            radio.send("0,1,0")
