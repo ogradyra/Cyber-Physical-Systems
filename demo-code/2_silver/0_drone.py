3b7b6fc9a598b4dc64f24663827571cc65043ad6
Daniel Flood <62947583+dflood99@users.noreply.github.com>
2021-12-08 14:39:16 +0000

    Update 0_drone.py

--- demo-code/2_silver/0_drone.py
+++ demo-code/2_silver/0_drone.py
@@ -11,30 +11,26 @@ uart.init(baudrate=115200, bits=8, parity=None, stop=1, tx=pin1, rx=pin2)
 radio.on()
 radio.config(length=251)
 radio.config(channel=47)
-radio.config(queue=5)
+radio.config(queue=1)
 micropython.kbd_intr(-1)
 
-x = 0
-y = 0
-z = 0
-a = 0
-t_flag = 0
-
+targetx  = 0
+targety  = 0
+height   = 0
+a        = 0
+t_flag   = 0
 Pitchtel = 0
 Rolltel  = 0
-
 incoming = 0
-ACK = utime.ticks_ms()
 
+ACK = utime.ticks_ms()
 buf = bytearray(16)
 
 def battery_read():
     battery = pin0.read_analog()
-    #print("Battery: ", battery)
     radio.send("1" + "," + "0" + "," + str(battery))
     
     charge = ((battery-300)/(1023-300))
-    #print(charge)
     if charge >= 0.6 and charge < 0.8:
         display.set_pixel(4, 0, 0)
         display.set_pixel(4, 1, 9)
@@ -68,62 +64,55 @@ def battery_read():
 
 # function for splitting up incoming strings and assigning values for p,a,r,t,y
 def receiver():
-    global x, y, z, a, t_flag
+    global targetx, targety, height, a, t_flag
 
     split_string = incoming.split(",")
     
     if split_string[0] == '0':
         if split_string[1] == '1':
-            print("ACK: ", split_string[2])
+            targetx = int(split_string[2])
+            targety = int(split_string[3])
+            height  = int(split_string[4])
+            a       = int(split_string[5])
+            t_flag  = int(split_string[6])
+        elif split_string[1] == '2':
             ack(split_string[2])
-        elif split_string[1] == '0':
-            x = int(split_string[2])
-            y = int(split_string[3])
-            z = int(split_string[4])
-            a = int(split_string[5])
-            t_flag = int(split_string[6])
-   
+
 def ack(signal):
     global ACK
     if signal == '1':
         ACK = utime.ticks_ms()
         display.set_pixel(2, 2, 9)
-
+ 
 def accelerometer_feedback():
     global Pitchtel, Rolltel
-    
-    #Rolltel = accelerometer.get_x()
-    #Pitchtel = -accelerometer.get_y()
-    
+
     if uart.any():
         data = uart.read()
         datalist = list(data)
-        #display.show(Image.HEART)
-        
         if isinstance(datalist, list) and len(datalist) >= 9:
             Pitchtel = int(datalist[3]) - int(datalist[4])
             Rolltel  = int(datalist[5]) - int(datalist[6])
-        
-    return Pitchtel, Rolltel
 
 # sends roll and pitch values to the montior
 def transmitter():
-    message = "2" + "," + "0" + "," + str(x) + "," + str(y)
+    message = "2" + "," + "0" + "," + str(Pitchtel) + "," + str(Rolltel)
     radio.send(message)
 
 # PID for X-direction control
 xE = 0
 xI = 0
-def xPID(xKp, xKi, xKd):
+def xPID(x, xKp, xKi, xKd, offset):
     global xE, xI
     
-    error = 0 - x
+    error = targetx + offset - x
   
     xD = error - xE #Difference between the errors (new - old error)
     
-    if abs(xD) > 5: #If error too big
-        error = xE #Error doesn't change
-        xD = 0 #Same so no difference to rate of change
+    #Low-Pass Filter
+    if abs(xD) > 5:
+        error = xE
+        xD = 0
     
     xP = error
     xI += error
@@ -142,23 +131,23 @@ def xPID(xKp, xKi, xKd):
 # PID for Y-direction control
 yE = 0
 yI = 0
-def yPID(yKp, yKi, yKd):
+def yPID(y, yKp, yKi, yKd, offset):
     global yE, yI
     
-    error = 0 - y
+    error = targety + offset - y
     
     yD = error - yE #Difference between the errors (new - old error)
     
-    if abs(yD) > 5: #If error too big
-        error = yE #Error doesn't change
-        yD = 0 #Same so no difference to rate of change
+    #Low-Pass Filter
+    if abs(yD) > 5:
+        error = yE 
+        yD = 0 
     
     yP = error
     yI += error
     
     yE = error
 
-    
     pitch = yKp*yP + yKi*yI + yKd*yD
     
     if pitch > 85:
@@ -167,16 +156,15 @@ def yPID(yKp, yKi, yKd):
         pitch = -85
     
     return pitch  
-    
+
 # PID for Z-direction control
 zE = 0
 zI = 0
-def zPID(zKp, zKi, zKd):
+def zPID(z, zKp, zKi, zKd):
     global zE, zI
-    target_height = 45 #Have target range instead of target height
-    #Have B pulses more than A pulses to counteract the lack of sensitivity for the downward pulses
-    error = target_height - z
-
+    
+    error = 45 - z
+    
     zP = error
     zI += error
     zD = error - zE
@@ -189,8 +177,8 @@ def zPID(zKp, zKi, zKd):
         throttle = 95
     if throttle < 0:
         throttle = 0
-    return throttle  
- 
+    
+    return throttle
    
 def driver(pitch, roll, throttle):
 
@@ -224,37 +212,34 @@ while True:
     incoming = radio.receive()
 
     if incoming:
-        print("Incoming: ",incoming)
         receiver()
-        x, y = accelerometer_feedback()
+        accelerometer_feedback()
         transmitter()
         
         if t_flag == 1:
-            throttle = zPID(2, 0, 0)
-            roll = xPID(0.5, 0.01, 1)
-            pitch = yPID(0.5, 0, 1)
+            roll     = xPID(Rolltel, 1, 0.01, 0.5, -2.2)
+            pitch    = yPID(Pitchtel, 1, 0.01, 0.5, 2.6)
+            throttle = zPID(height, 2, 0, 0)
         else:
+            roll     = 0
+            pitch    = 0
             throttle = 0
-            roll = 0
-            pitch = 0
             zE = 0
             zI = 0
             xE = 0
             xI = 0
             yE = 0
             yI = 0
-
+        
         if a > 0:
             display.set_pixel(0, 0, 9)
-        else:
+        else: 
             display.set_pixel(0, 0, 0)
-            
+        
         driver(pitch, roll, throttle)
         battery_read()
-        sleep(10)
-    #else:
-        #print("No Incoming Data")
-        
+        sleep(50)
+    
     if utime.ticks_diff(utime.ticks_ms(), ACK) >= 1000 or utime.ticks_diff(utime.ticks_ms(), ACK) < 0:
         #print("reset")
         display.set_pixel(2, 2, 0) # reset d, so this code can execute every 50 ms
