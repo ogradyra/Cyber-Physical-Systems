a9311ade766bc7e319ada5c983f7bf6aff8c9458
Daniel Flood <62947583+dflood99@users.noreply.github.com>
2021-12-08 11:54:14 +0000

    Code Cleanup

---
+++ weekly-code/week12/2_monitor.py
@@ -0,0 +1,32 @@
+# Add your Python code here. E.g.
+from microbit import *
+import radio
+import micropython
+
+radio.on()
+radio.config(length=251)
+radio.config(channel=47)
+radio.config(queue=1)
+
+while True:
+    incoming = radio.receive()
+    
+    if incoming:
+        #print("Incoming: ",incoming)
+        string = incoming.split(",")
+        #print(string)
+        address = int(string[0])
+        if address == 2:
+            if string[1] == '0':
+                pitch = int(string[2])
+                roll  = int(string[3])
+                print("Pitch: ", pitch, " Roll: ", roll)
+                radio.send("0" + "," + "1" + "," + "1")
+                sleep(3000)
+                
+                
+                
+'''     else:
+                radio.send("0"+ "," + "1" + "," + "0")
+        else:
+            radio.send("0" + "," + "1" + "," + "0")'''
